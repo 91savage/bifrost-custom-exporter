4d0842f6fcc67965a8df4891047d5bdbae9ac1d0
91savage <ekdma1403@gmail.com>
2023-07-08 16:48:09 +0900

    label 등록 끝

--- bifrost_balance.py
+++ bifrost_balance.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import time
 
-from prometheus_clinet.core import GaugeMetricFamily
+from prometheus_client.core import GaugeMetricFamily
 from prometheus_client import start_http_server, Gauge
 
 load_dotenv()
@@ -15,35 +15,34 @@ controllerAddress = os.environ.get('controllerAddress')
 w3 = Web3(Web3.HTTPProvider('https://public-01.mainnet.thebifrost.io/rpc'))
 balance = round(w3.from_wei(w3.eth.get_balance(relayerAddress),'ether'),2)
 
+s_balance = round(w3.from_wei(w3.eth.get_balance(stashAddress),'ether'),2)
+c_balance = round(w3.from_wei(w3.eth.get_balance(controllerAddress),'ether'),2)
+r_balance = round(w3.from_wei(w3.eth.get_balance(relayerAddress),'ether'),2)
 
-rBalance = Gauge(
+
+Balance = Gauge(
     'balance',
-    'This is Bifrost Balance'
+    'This is Bifrost Balance',
+    ["stash", "controller","relayer"]
 )
 
-def balance():
+# def get_stash_balance():
+#     balance = round(w3.from_wei(w3.eth.get_balance(stashAddress),'ether'),2)
+#     return balance
 
-    balance = GaugeMetricFamily("Bifrost_Balance", "This is Balance of Bifrost", labels =["Stash","Controller","Relayer"])
 
-def get_stash_balance():
-    balance = round(w3.from_wei(w3.eth.get_balance(stashAddress),'ether'),2)
-    return balance
 
-def get_relayer_balance():
-    balance = round(w3.from_wei(w3.eth.get_balance(relayerAddress),'ether'),2)
-    return balance
 
-def get_controller_balance():
-    balance = round(w3.from_wei(w3.eth.get_balance(controllerAddress),'ether'),2)
-    return balance
 
 if __name__ == "__main__":
     port = 9000
     frequency =1
 
+    Balance.labels(stash=f'{s_balance}',controller=f'{c_balance}', relayer=f'{r_balance}')
 
+    ## Exporter 서버 시작
     start_http_server(port)
 
     while True:
-        rBalance.set(get_relayer_balance())
+        # Balance.set(get_stash_balance())
         time.sleep(frequency)
